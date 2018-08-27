4aa82b3b083d6ba3e28e54266db69b9000b2d952
Olha Melska <olya.melska@gmail.com>
2018-08-27 17:48:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file
+let drivers = ['Bob', 'Daniel', 'Sammy', 'Nathan', 'David', 'Angela', 'Sarah', 'Sally', 'Billy']
+
+function fuzzyMatch(drivers, word) {
+  return drivers.filter(function(el) {
+      return el.toLowerCase().indexOf(word.toLowerCase()) > -1;
+  })
+}
+
+console.log(fuzzyMatch(drivers, 'b')); 
+
+function findMatching(drivers, word){
+  return drivers.filter( function (el){
+    if(el === word){
+      return el;
+    }
+    else if(el.toLowerCase() === word.toLowerCase()){
+      return el;
+    }
+    else{
+      return false;
+    }
+  })
+}
+console.log(findMatching(drivers, 'Daniel'))
+
+
+function matchName(drivers, name){
+  return drivers.filter(function(el){
+    if(el.name === name){
+      return name;
+    }
+    else{
+      return false
+    }
+  })
+}
